4c2b8b6d5cfc14919f12e877928d3e0bdc5e51f0
“NaimKhanSiam” <naim_khan@live.co.uk>
2018-05-19 13:28:32 -0400

    Saturday

---
+++ Ebay/src/test/java/home/HomePageTest.java
@@ -0,0 +1,12 @@
+package home;
+
+import methods.HomePage;
+import org.testng.annotations.Test;
+
+public class HomePageTest extends HomePage {
+    @Test
+    public void SideBar(){
+
+        sideBar();
+    }
+}
